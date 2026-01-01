30d4caee450bbb5a6afb8a87251de161602fbc51
lukas-sgx <lukas.soigneux@epitech.eu>
2026-01-02 00:13:00 +0100

    [FIX] Coding style

--- main.c
+++ main.c
@@ -9,32 +9,49 @@
 #include <stdio.h>
 #include "epiclang.h"
 
+int add_arg(char **cmd, char *arg)
+{
+    char *new_buffer = realloc(*cmd, sizeof(char) *
+        (strlen(*cmd) + strlen(arg) + 2));
+
+    if (new_buffer == NULL)
+        return 84;
+    *cmd = new_buffer;
+    strcat(*cmd, " ");
+    strcat(*cmd, arg);
+    return 0;
+}
+
+int add_plugin(char **cmd)
+{
+    char *new_buffer = realloc(*cmd, sizeof(char) * (strlen(*cmd)
+            + strlen(" -fplugin=" EPICLANG_PLUGIN_PATH) + 1));
+
+    if (new_buffer == NULL) {
+        free(*cmd);
+        return 84;
+    }
+    *cmd = new_buffer;
+    strcat(*cmd, " -fplugin=" EPICLANG_PLUGIN_PATH);
+    system(*cmd);
+    free(*cmd);
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     char *cmd = malloc(sizeof(char) * (strlen(EPICLANG_COMPILER) + 1));
-    char *new_buffer = NULL;
 
     if (cmd == NULL)
         return 84;
     strcpy(cmd, EPICLANG_COMPILER);
     for (int i = 1; i < argc; i++) {
-        new_buffer = realloc(cmd, sizeof(char) * (strlen(cmd) + strlen(argv[i]) + 2));
-        if (new_buffer == NULL) {
+        if (add_arg(&cmd, argv[i]) != 0) {
             free(cmd);
             return 84;
         }
-        cmd = new_buffer;
-        strcat(cmd, " ");
-        strcat(cmd, argv[i]);
     }
-    new_buffer = realloc(cmd, sizeof(char) * (strlen(cmd) + strlen(" -fplugin=" EPICLANG_PLUGIN_PATH) + 1));
-    if (new_buffer == NULL) {
-        free(cmd);
+    if (add_plugin(&cmd) != 0)
         return 84;
-    }
-    cmd = new_buffer;
-    strcat(cmd, " -fplugin=" EPICLANG_PLUGIN_PATH);
-    system(cmd);
-    free(cmd);
     return 0;
 }
