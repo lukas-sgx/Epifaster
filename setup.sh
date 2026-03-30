2416f141b3372ddf0b9a854d470bf4caf8150c02
lukas-sgx <lukas.soigneux@epitech.eu>
2026-03-30 23:20:08 +0200

    [~] Decomente line -> create epiclang

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@ if [ ! -d /usr/lib/ ]
 then
     ./nix/setup.sh
 else
-    # sudo mkdir -p /usr/lib/epiclang/
-    # sudo cp -r ./plugins /usr/lib/epiclang/
-    # make install
-    # chmod +x epiclang
-    # sudo cp epiclang /usr/bin/epiclang
+    sudo mkdir -p /usr/lib/epiclang/
+    sudo cp -r ./plugins /usr/lib/epiclang/
+    make install
+    chmod +x epiclang
+    sudo cp epiclang /usr/bin/epiclang
     echo "Installation complete. Plugins have been copied to /usr/lib/epiclang/plugins/"
 fi
